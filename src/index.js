c23167d08c99593994c07a385a6ee9c60220bdfb
Andres Betancourt <aebb001@gmail.com>
2021-02-12 22:50:53 -0600

    Hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import "./assets/styles/styles.css";
-console.log("Aqui estoy yo dandole");
+
+const openBurgerMenu =
+  "https://firebasestorage.googleapis.com/v0/b/woorki-ve.appspot.com/o/icon-hamburger.svg?alt=media&token=cc5191a7-2762-43ef-af85-6fa72ce027ff";
+const closeBurgerMenu =
+  "https://firebasestorage.googleapis.com/v0/b/woorki-ve.appspot.com/o/icon-close.svg?alt=media&token=3eab5dc9-204b-4402-aaad-10ae6e004c56";
+
+const button = document.getElementById("burger-boton");
+const menu = document.getElementById("menu");
+
+button.onclick = () => {
+  console.log("hola");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    button.src = openBurgerMenu;
+  } else {
+    menu.style.display = "block";
+    button.src = closeBurgerMenu;
+  }
+};
